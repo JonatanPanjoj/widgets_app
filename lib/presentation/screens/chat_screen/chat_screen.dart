e7aa5d045bc1b3d0e54c09f143ae1e1e85b56b02
Jonatan Josué Ixpanel Panjoj <josuejonatan142@gmail.com>
2023-06-30 17:12:21 -0600

    api_key_added

--- lib/presentation/screens/chat_screen/chat_screen.dart
+++ lib/presentation/screens/chat_screen/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/constants/environment.dart';
 
 class ChatScreen extends StatelessWidget {
 
@@ -8,6 +9,13 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat Screen'),
+      ),
+      body: Center(
+        child: Text(Environment.gptKey),
+      ),
+    );
   }
 }
